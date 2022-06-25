f7860a19103b0e962ce8efa80c43bb16b4be7fab
Grigorii Podoksik <h.podoksik@gmail.com>
2022-06-25 10:33:08 -0400

    Implemented multithreading for trading, model update, data retrieval

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 import logging
-from time import sleep
+from time import sleep, time
 from threading import Thread, Event
 import numpy as np
+import math
 
 from neural_network import NeuralNet
 from sapi import Sapi
 
+
+def update_price_data(sapi, interval, trials_amount, save_interval=50, data_path='saved_data/data.csv'):
+    while True:
+        for i in range(math.ceil(trials_amount / save_interval)):
+            sapi.collect_data(save_interval, interval, 'append')
+            sapi.export_data(data_path, interval)
+
+
+def train_model(model_creator, sapi, measurement_interval, data_path, delay):
+    while True:
+        try:
+            model_creator.train(data_path)
+            training_ended_time = time()
+            while True:
+                if time() <= training_ended_time + delay:
+                    break
+                print(
+                    f'\u001b[32mPrediction {model_creator.prediction_delay * measurement_interval} seconds forward is {model_creator.predict(np.array([sapi.ask_prices, sapi.bid_prices]))}\u001b[0m')
+                sleep(model_creator.prediction_delay * measurement_interval)
+                print(f'\u001b[32mCurrent prices are: {sapi.ask_prices[-1], sapi.bid_prices[-1]}\u001b[0m\n\n')
+        except Exception as err:
+            print(err)
+
+
 if __name__ == '__main__':
-    interval = 10
+    interval = 15
+    trials_amount = 1000
 
     logging.basicConfig()
     logger = logging.getLogger()
@@ -20,24 +46,38 @@ if __name__ == '__main__':
         credentials['totp'] = raw_data[2].strip()
 
     btc_sapi = Sapi(credentials, 'BTC', 5, logger=logger)
-    # mode = int(input('Enter mode'))
     # if mode == 0:
     #     for i in range(2000):
     #         print(f'{i}/2000')
     #         btc_sapi.collect_data(15, interval, 'append')
-    #         btc_sapi.export_data('data.csv', interval)
+    #         btc_sapi.export_data('data_saved0.csv', interval)
     #
     #     model = NeuralNet()
     #     model.train()
     #     data = np.array([btc_sapi.ask_prices[-model.trail_size], btc_sapi.bid_prices[-model.trail_size]])
     # elif mode == 1:
-    #     data = btc_sapi.read_file('data.csv').to_numpy()
+    #     data = btc_sapi.read_file('data_saved0.csv').to_numpy()
     #     print(data)
     #     model = NeuralNet()
     #     model.train()
 
-    # Create a price updating thread
-    # Run it N times, if needed, to generate initial data
+    mode = int(input('Enter mode'))
+    if mode == 0:
+        # Create a price updating thread
+        btc_sapi.collect_data(trials_amount, interval, 'erase')
+    elif mode == 1:
+        data = btc_sapi.read_file('data_saved0.csv').to_numpy()
+        btc_sapi.ask_prices, btc_sapi.bid_prices = data[0], data[1]
+
     # Reset the price updating thread to run constantly on append
+    data_generation_thread = Thread(target=update_price_data, args=[btc_sapi, interval, trials_amount])
+    data_generation_thread.start()
+
     # Run model-updating thread constantly
-    # Run prediction-and-trading thread constantly
+    model_creator = NeuralNet(200, 500)
+    model_update_thread = Thread(target=train_model, args=[model_creator, btc_sapi, interval, 'saved_data/data.csv', 1200])
+    model_update_thread.start()
+
+    # # Run prediction-and-trading thread constantly
+    # while True:
+    #
--- neural_network.py
+++ neural_network.py
@@ -10,24 +10,18 @@ import pandas
 from sklearn.model_selection import train_test_split
 
 
-
 class NeuralNet:
-    trail_size = 50
-    prediction_delay = 30
-    batch_size = 32
-    epochs = 10
+    model = None
 
-    model = Sequential([
-        Input(shape=(2, trail_size)),
-        LSTM(300, activation='relu', return_sequences=True),
-        LSTM(300, activation='relu', return_sequences=False),
-        Dense(100, input_dim=300, activation='relu'),
-        Dense(100, input_dim=100, activation='relu'),
-        Dense(2, input_dim=100, activation='relu')
-    ])
+    def __init__(self, trail_size, prediction_delay, batch_size=32, epochs=10):
+        self.trail_size = trail_size
+        self.prediction_delay = prediction_delay
+        self.batch_size = batch_size
+        self.epochs = epochs
 
     def pre_model_transform(self, data, **kwargs):
-        #return np.vectorize(math.log)(data)
+        # return np.vectorize(math.log)(data)
+
         # avg_len = kwargs['avg_len']
         # if kwargs['arg_type'] == 'x':
         #     transformed_data = []
@@ -35,65 +29,70 @@ class NeuralNet:
         #         transformed_data.append((data[i:i+avg_len][0].mean(), data[i:i+avg_len][1].mean()))
         #     return np.array(transformed_data)
         # return data[:-avg_len]
+
         return data
 
     def post_model_transform(self, data, **kwargs):
-        #return np.vectorize(math.exp)(data)
+        # return np.vectorize(math.exp)(data)
+
         return data
 
-    def train(self):
-        data = pandas.read_csv('saved_data/data_saved2.csv', sep=',', index_col=False, header=2)
+    def train(self, data_path, max_tolerable_err=400, min_runs=5):
+        data = pandas.read_csv(data_path, sep=',', index_col=False, header=2)
 
         x = self.pre_model_transform(np.array([(
             data['ask'].iloc[i:i + self.trail_size],
             data['bid'].iloc[i:i + self.trail_size]
-        ) for i in range(data.shape[0] - self.trail_size - self.prediction_delay)]), arg_type='x', avg_len=20)
+        ) for i in range(data.shape[0] - self.trail_size - self.prediction_delay)]))
 
         y = self.pre_model_transform(np.array([(
             data['ask'].iloc[i + self.prediction_delay],
             data['bid'].iloc[i + self.prediction_delay]
-        ) for i in range(self.trail_size, data.shape[0] - self.prediction_delay)]), arg_type='y', avg_len=20)
+        ) for i in range(self.trail_size, data.shape[0] - self.prediction_delay)]))
 
-        x_train, x_test, y_train, y_test = train_test_split(x, y, shuffle=True, test_size=0.2)
-        self.model.compile(loss='mean_squared_logarithmic_error', optimizer=Adagrad(0.005, decay=0.0002),
-                           metrics=['accuracy', 'mean_absolute_error'])
-        history = self.model.fit(x_train, y_train, epochs=self.epochs, batch_size=self.batch_size, verbose=1).history
+        run = 0
+        best_model = None
+        best_performance = math.inf
+        while run < min_runs:
+            x_train, x_test, y_train, y_test = train_test_split(x, y, shuffle=True, test_size=0.2)
+            model = Sequential([
+                Input(shape=(2, self.trail_size)),
+                LSTM(300, activation='relu', return_sequences=True),
+                LSTM(300, activation='relu', return_sequences=False),
+                Dense(100, input_dim=300, activation='relu'),
+                Dense(100, input_dim=100, activation='relu'),
+                Dense(2, input_dim=100, activation='relu')
+            ])
+            model.compile(loss='mean_squared_logarithmic_error', optimizer=Adagrad(0.005, decay=0.0002),
+                          metrics=['accuracy', 'mean_absolute_error'])
+            history = model.fit(x_train, y_train, epochs=self.epochs, batch_size=self.batch_size, verbose=1).history
 
-        transformed_x_test, transformed_y_test = self.post_model_transform(x_test), self.post_model_transform(y_test)
+            transformed_x_test, transformed_y_test = self.pre_model_transform(x_test), self.pre_model_transform(
+                y_test)
+
+            mean_absolute_error = history['mean_absolute_error'][-1]
+            if mean_absolute_error <= max_tolerable_err:
+                run += 1
+                if mean_absolute_error < best_performance:
+                    best_performance = mean_absolute_error
+                    best_model = model
+
         fig, ax = plt.subplots()
-
+        transformed_x, transformed_y = self.pre_model_transform(x), self.pre_model_transform(y)
         get_last_arg = lambda np_array: np_array[:, :, -1]
-        ax.plot(get_last_arg(transformed_x_test), transformed_y_test, 'ro')
-        ax.plot(get_last_arg(transformed_x_test), self.model.predict(transformed_x_test), 'bo')
-        # errors = np.empty((0, 2), float)
-        # for i, curr_x, curr_pred, curr_y in zip(range(len(x)), self.post_model_transform(x), self.post_model_transform(self.model.predict(x)), self.post_model_transform(y)):
-        #     is_test_value = curr_x in transformed_x_test and curr_y in transformed_y_test
-        #     styles = ['ro', 'bo']
-        #     if is_test_value:
-        #         errors = np.append(np.array(errors, ndmin=2), [curr_pred - curr_y], axis=0)
-        #         print(f"Test {i + 1}:\n\t"
-        #               f"X (last): {curr_x[:, -1]}\n\t"
-        #               f"X (first): {curr_x[:, 0]}\n\t"
-        #               f"Prediction: {curr_pred}\n\t"
-        #               f"Actual value: {curr_y}\n\t"
-        #               f"Error: {np.array(errors, ndmin=2)[-1, :] if errors.size != 0 else '-'}")
-        #
-        #         ax.plot(i, curr_x[:, -1][0], styles[0], markeredgewidth=0.001)
-        #         # ax.plot(i, curr_x[:, -1][1], styles[0], markeredgewidth=0.001)
-        #         ax.plot(i + self.prediction_delay, curr_pred[0], styles[1], markeredgewidth=0.001)
-        #         # ax.plot(i + self.prediction_delay, curr_pred[1], styles[1], markeredgewidth=0.001)
-
-        # abs_errors = np.absolute(np.array(errors))
-        # print('dim 0', abs_errors[:, 0].mean(), abs_errors[:, 0].std())
-        # print('dim 1', abs_errors[:, 1].mean(), abs_errors[:, 1].std())
+        ax.plot(get_last_arg(transformed_x), transformed_y, 'ro')
+        ax.plot(get_last_arg(transformed_x), best_model.predict(transformed_x), 'bo')
         plt.show()
-        print(history['mean_absolute_error'])
+        self.model = best_model
 
     def predict(self, data):
-        transformed_data = self.pre_model_transform(data)
+        transformed_data = self.pre_model_transform(
+            np.array([data[:, i:i + self.trail_size] for i in range(data.shape[1] - self.trail_size)])
+        )
         returned_data = self.model.predict(transformed_data)
-        return self.post_model_transform(returned_data)
+        return self.post_model_transform(returned_data)[:, :, -1]
+
 
 if __name__ == '__main__':
-    model = NeuralNet()
-    model.train()
+    model_handler = NeuralNet()
+    model_handler.train('saved_data/data_saved2.csv', 250, 10)
